b730779710d434efed318330a525a7735316a158
savitha1 <59514085+savitham1@users.noreply.github.com>
2020-05-01 14:25:48 -0400

    Insertion sort algorith

--- Insertionsort.c
+++ Insertionsort.c
@@ -1,4 +1,14 @@
 //INSERTION SORT
+/**
+	Insertion sort of an Positional List 
+
+Uses 3 pointers:
+	1. marker - holds the current position at the list
+	2. pivot - holds the new value next to the marker 
+	3. walk - navigates through the sorted list behind the marker and finds the postion to add pivot.
+
+**/
+
 
 #include <stdio.h>
 
@@ -12,19 +22,30 @@ int main(void)
 
     for (int i = 0; i < count - 1; i++)
     {
-        s = i;
+        s = i;  // s is the marker
 
-        // Swap the largest element
-        if (arr[i] > arr[i + 1])
+        
+        if (arr[s] > arr[s + 1])
+        // If the current element is greater than the next element
+        // s+1 is the pivot or the element next to marker.
         {
+            //Swap those elements
             int temp = arr[i];
-            arr[i] = arr[i + 1];
-            arr[i + 1] = temp;
+            arr[s] = arr[s + 1];
+            arr[s + 1] = temp;
 
-            // Sort the arrays before the current swap
+            /**
+            After the above swap,
+            we must also sort the elements before 's'.
+            So that all the element before the marker (s) is sorted
+            **/
             for (int n = s - 1; n >= 0; n--, s--)
+            // n is a 'walk' and will navigate through the elements before 'marker'
             {
                 if (arr[s] < arr[n])
+                // When marker is lesser than walk, it implies that 
+                // the list is still not sorted
+                // Swap those elements
                 {
                     int temp1 = arr[s];
                     arr[s] = arr[n];
@@ -39,5 +60,7 @@ int main(void)
         printf("%i \n", arr[i]);
     }
 }
+
+
 
 
